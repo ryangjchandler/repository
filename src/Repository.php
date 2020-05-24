fee36f5c7b90656b4919f027bd33816631a5d435
Ryan Chandler <ryan@chandler.codes>
2020-05-24 18:45:04 +0100

    wip(static var)

--- src/Repository.php
+++ src/Repository.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\App;
 
 abstract class Repository
 {
-    protected string $model;
+    protected static string $model;
 
     public function __call(string $name, array $arguments)
     {
-        return App::make($this->model)->{$name}(...$arguments);
+        return App::make(static::$model)->{$name}(...$arguments);
     }
 }
